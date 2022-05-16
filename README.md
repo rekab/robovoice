83e6f38d26bde602f390b49b461febbf4a492860
pi@rpi4-dev0 <pi@rpi4-dev0>
2022-05-15 17:49:43 -0700

    add lightbar support

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 https://docs.google.com/document/d/1wDfdpEdLlfDwexNORDDVtKARrnzqxjQtfFlcrvqhabM/edit
 
+sudo apt install libportaudio2
+sudo apt install libportmidi0
+sudo apt install liblo-tools
+sudo apt install libsndfile1
 
+python3 -m venv voice-env
 source voice-env/bin/activate
 pip install -r requirements.txt
--- robovoice.py
+++ robovoice.py
@@ -1,5 +1,43 @@
+import adafruit_dotstar as dotstar
+import board
 import pyo
+import threading
+import time
 
+NUM_PIXELS = 73
+BRIGHTNESS = 0.5
+
+# Setup the light bar
+dots = dotstar.DotStar(
+        board.SCK, board.MOSI, NUM_PIXELS, brightness=BRIGHTNESS)
+dots.fill((0, 0, 0))
+
+shutdown = False
+last_pixel = 0
+prev_last_pixel = 0
+
+def setAmplitude(*args):
+    global last_pixel
+    mag = max(args) * 2
+    last_pixel = min(NUM_PIXELS, int(mag * NUM_PIXELS))
+    #print('mag=%s last_pixel=%s' % (mag, last_pixel))
+
+def colorBar():
+    print('colorBar() starting')
+    global last_pixel, prev_last_pixel, shutdown
+    while not shutdown:
+        if last_pixel != prev_last_pixel:
+            print('change: %d' % (prev_last_pixel - last_pixel))
+            for idx in range(0, last_pixel):
+                dots[idx] = (255, 0, 0)
+            for idx in range(last_pixel, NUM_PIXELS):
+                dots[idx] = (0, 0, 0)
+            prev_last_pixel = last_pixel
+        time.sleep(.02)
+
+
+
+# Setup pyo
 s = pyo.Server()
 s.setInputDevice(1)
 s.setOutputDevice(1)
@@ -15,11 +53,13 @@ mm.addInput(0, f)
 mm.setAmp(0, 0, 0.5)
 output = pyo.Sig(mm[0]).out()
 
-def what(*args):
-    print(args)
+amp = pyo.PeakAmp(output, function=setAmplitude)
 
-amp = pyo.PeakAmp(output, function=what)
+t = threading.Thread(target=colorBar)
+t.start()
 
 s.start()
 import code
 code.interact(local=locals())
+shutdown = True
+t.join()
