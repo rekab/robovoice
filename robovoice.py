86587e397588a7d37c8947bdfa22e6ec8f641398
pi@rpi4-dev0 <pi@rpi4-dev0>
2022-05-17 21:58:05 -0700

    refactor into a class; make a gradient bar fill

--- robovoice.py
+++ robovoice.py
@@ -1,99 +1,128 @@
 import adafruit_dotstar as dotstar
 import board
 import pyo
+import queue
 import threading
 import time
 
 NUM_PIXELS = 73
 BRIGHTNESS = 0.5
 
-# Setup the light bar
-dots = dotstar.DotStar(
-        board.SCK, board.MOSI, NUM_PIXELS, brightness=BRIGHTNESS)
-dots.fill((0, 0, 0))
-
-shutdown = False
-last_pixel = 0
-prev_last_pixel = 0
-
-def setAmplitude(*args):
-    global last_pixel
-    mag = max(args) * 2
-    last_pixel = min(NUM_PIXELS, int(mag * NUM_PIXELS))
-    #print('mag=%s last_pixel=%s' % (mag, last_pixel))
-
-def colorBar():
-    print('colorBar() starting')
-    global last_pixel, prev_last_pixel, shutdown
-    while not shutdown:
-        if last_pixel != prev_last_pixel:
-            #print('change: %d' % (prev_last_pixel - last_pixel))
-            for idx in range(0, last_pixel):
-                dots[idx] = (255, 0, 0)
-            for idx in range(last_pixel, NUM_PIXELS):
-                dots[idx] = (0, 0, 0)
-            prev_last_pixel = last_pixel
-        time.sleep(.01)
+# Number of pixels that "fade" in.
+GRADIENT_WIDTH = 5
 
 
+class ColorBar(object):
+    def __init__(self):
+        self._shutdown = False
+        self.q = queue.Queue()
+        # Setup the light bar
+        self.dots = dotstar.DotStar(
+                board.SCK, board.MOSI, NUM_PIXELS, brightness=BRIGHTNESS)
+        self.dots.fill((0, 0, 0))
 
-# Setup pyo
-s = pyo.Server()
-s.setInputDevice(1)
-s.setOutputDevice(1)
-s.boot()
+        self.t = threading.Thread(target=self._colorBar)
 
-mic = pyo.Input(chnl=0)
+    def start(self):
+        print('starting thread...')
+        self.t.start()
 
-h = pyo.Harmonizer(mic, transpo=5, feedback=0.2, winsize=0.2)
-#output = f.mix(2).out()
-mix = h.mix(2)
+    def setAmplitude(self, *args):
+        """Should be called from the main thread."""
+        mag = max(args) * 2
+        self.q.put(mag)
+        #print('mag=%s last_pixel=%s' % (mag, last_pixel))
 
-feedback = pyo.Sig(0.7, mul=0.95)  # 0 --> 1
-flg = pyo.Delay(pyo.DCBlock(mix), delay=.02, feedback=feedback)
+    def _colorBar(self):
+        print('_colorBar() starting')
+        prev_value = last_pixel = 0
+        while not self._shutdown:
+            mag = self.q.get(block=True)
+            #print('got %s' % mag)
+            last_pixel = min(NUM_PIXELS, int(mag * NUM_PIXELS))
+            if last_pixel == prev_value:
+                # No change: Don't waste time writing to the bar.
+                continue
+            prev_value = last_pixel
+            #print('change: %d' % (prev_value - last_pixel))
+            #for idx in range(0, last_pixel):
+            #    self.dots[idx] = (255, 0, 0)
+            #for idx in range(last_pixel, NUM_PIXELS):
+            #    self.dots[idx] = (0, 0, 0)
+            #prev_last_pixel = last_pixel
+            self._fillBar(last_pixel)
 
-# Mix the original source with its delayed version.
-# Compress the mix to normalize the output signal.
-output = pyo.Compress(mix + flg, thresh=-20, ratio=4).out()
+    def _fillBar(self, num_pixels):
+        up_gradient = [
+                # (r, g, b) values: gets whiter
+                (10+p, (p*p)//500, (p*p)//500)
+                # Step from 0 to 255
+                for p in range(0, 255, 255//GRADIENT_WIDTH)]
 
-#
-######
-## From: http://ajaxsoundstudio.com/pyodoc/examples/07-effects/05-hand-made-chorus.html
-## Sets values for 8 LFO'ed delay lines (you can add more if you want!).
-## LFO frequencies.
-#freqs = [0.254, 0.465, 0.657, 0.879, 1.23, 1.342, 1.654, 1.879]
-## Center delays in seconds.
-#cdelay = [0.0087, 0.0102, 0.0111, 0.02254, 0.0134, 0.01501, 0.01707, 0.0178]
-## Modulation depths in seconds.
-#adelay = [0.009, 0.0042, 0.0043, 0.0014, 0.0015, 0.0016, 0.002, 0.0023]
-#
-## Create 8 sinusoidal LFOs with center delays "cdelay" and depths "adelay".
-#lfos = pyo.Sine(freqs, mul=adelay, add=cdelay)
-#
-## Create 8 modulated delay lines with a little feedback and send the signals
-## to the output. Streams 1, 3, 5, 7 to the left and streams 2, 4, 6, 8 to the
-## right (default behaviour of the out() method).
-#d = pyo.Delay(compressed, lfos, feedback=0.9, mul=0.7)
-#
-#lfo4 = pyo.Sine(0.5).range(0.1, 3.75)
-#osc4 = pyo.SuperSaw(freq=187.5, detune=lfo4, mul=1)
-#
-#output = pyo.Selector([d, osc4]).out()
+        between_color = (20, 255, 0)
 
-#####
+        down_gradient = up_gradient[:]
+
+        # Start with a black bar.
+        color_pixels = [(0, 0, 0)] * NUM_PIXELS
+        
+        # Where we start and end coloring in.
+        color_fill_start = max((NUM_PIXELS//2) - (num_pixels//2), 0)
+        color_fill_end = min(color_fill_start + num_pixels, NUM_PIXELS - 1)
+        #print(f'start={color_fill_start} end={color_fill_end} max={NUM_PIXELS}')
+
+        while color_fill_start <= color_fill_end:
+            if up_gradient:
+                color_pixels[color_fill_start] = up_gradient.pop(0)
+            else:
+                color_pixels[color_fill_start] = between_color
+            color_fill_start += 1
+            if down_gradient:
+                color_pixels[color_fill_end] = down_gradient.pop(0)
+            else:
+                color_pixels[color_fill_end] = between_color
+            color_fill_end -= 1
+
+        for idx in range(0, len(color_pixels)):
+            self.dots[idx] = color_pixels[idx]
 
 
-# Feed peak amp to setAmplitude
-amp = pyo.PeakAmp(output, function=setAmplitude)
-# TODO: polltime(.02)?
+    def shutdown(self):
+        print('shutting down')
+        self._shutdown = True
+        self.q.put(0)
+        self.t.join()
 
-# http://ajaxsoundstudio.com/pyodoc/api/classes/generators.html#pyo.SuperSaw
 
-t = threading.Thread(target=colorBar)
-t.start()
+def main():
+    # Setup pyo
+    s = pyo.Server()
+    s.setInputDevice(1)
+    s.setOutputDevice(1)
+    s.boot()
 
-s.start()
-import code
-code.interact(local=locals())
-shutdown = True
-t.join()
+    mic = pyo.Input(chnl=0)
+
+    h = pyo.Harmonizer(mic, transpo=5, feedback=0.2, winsize=0.2)
+    mix = h.mix(2)
+
+    feedback = pyo.Sig(0.7, mul=0.95)
+    flg = pyo.Delay(pyo.DCBlock(mix), delay=.02, feedback=feedback)
+
+    # Mix the original source with its delayed version.
+    # Compress the mix to normalize the output signal.
+    output = pyo.Compress(mix + flg, thresh=-20, ratio=4).out()
+
+    # Feed peak amp to setAmplitude
+    cb = ColorBar()
+    amp = pyo.PeakAmp(output, function=cb.setAmplitude)
+    # TODO: polltime(.02)?
+
+    cb.start()
+    s.start()
+    import code
+    code.interact(local=locals())
+    cb.shutdown()
+
+if __name__ == '__main__':
+    main()
